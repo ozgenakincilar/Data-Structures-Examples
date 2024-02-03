eb3a9c2f4b0cff450fc707f5c389298808667546
Özgen AKINCILAR <ozgen.akincilar@veripark.com>
2024-02-04 01:04:44 +0300

    Fibonacci Example

---
+++ DynamicProgramming/FibonacciNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FibonacciNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please write the input ");
+            var input = Console.ReadLine();
+            var result = Fibonacci(Convert.ToInt32(input));
+            Console.WriteLine($" result is : {result}");
+        }
+
+        public static int Fibonacci(int number)
+        {
+            if (number <= 1)
+            {
+                return number;
+            }
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+        }
+    }
+}
+
